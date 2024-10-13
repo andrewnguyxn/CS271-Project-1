febed3e92174789825a436fe5b4832f0b47dc7c1
Huy_Phan0511 <phan_h3@red7.cs.denison.edu>
2024-10-13 17:53:53 -0400

    Adding basic stuff

---
+++ sorting.cpp
@@ -0,0 +1,42 @@
+//==============================================================
+// Names: Huy Phan, Tri Dang, Andrew Nguyen
+// Class: CS 271-01
+// Project 1:
+// Date: 10/13/2024
+// About: DoublyLinkedList.cpp contains the implementations of
+// the methods for the DoublyLinkedList class, as defined in
+// the header file DoublyLinkedList.hpp.
+//==============================================================
+#include <iostream>
+#include <stdlib.h>
+#include "DoublyLinkedList.hpp"
+
+#include "sorting.hpp"
+
+// Definition for insertion sort
+template<typename T>
+    void insertionSort(T* arr, long n) {
+    
+}
+
+// Definition for merge sort
+template<typename T>
+    void mergeSort(T* arr, long n) {
+    
+}
+
+// Definition for quicksort
+template<typename T>
+    void quicksort(T* arr, long n) {
+    
+}
+
+// Definition for improved quicksort
+template<typename T>
+    void improvedQuicksort(T* arr, long n) {
+    
+}
+
+// Definition for randomized quicksort
+template<typename T>
+    void randomizedQuicksort(T* arr, long n);
